6eee67f9d19386cebe72f60852a717b1dd77b86b
Stu Campbell <stucampbell.git@gmail.com>
2019-10-16 17:53:41 -0700

    added CLI arg to dictate audio or vid download... implemented a full test run

--- canvasscraper/fileops/DirMaker.py
+++ canvasscraper/fileops/DirMaker.py
@@ -1,13 +1,18 @@
 import os
+from canvasscraper.fileops.Downloader import Downloader
 
 
 class DirMaker:
 
-    def __init__(self, root_dir=None):
+    def __init__(self, root_dir=None, formattype=None):
         if root_dir is None:
             self.root_dir = os.getcwd()
         else:
             self.root_dir = root_dir
+        if formattype is None:
+            self.formattype = 'mp4'
+        else:
+            self.formattype = formattype
         self.new_base_dir = self.root_dir + "/Courses"
 
     def __make_base_dir(self):
@@ -40,12 +45,13 @@ class DirMaker:
             self.__make_course_dir(course_obj.name)
             for name, obj in course_obj.pages.items():
                 if obj.has_vids():
-                    print(f"Page{name} has links, making Page Folder")
-                    self.__make_page_dir(course_obj.name, name)
-                    os.mkdir(self.new_base_dir + f"/{course_obj.name}/{name}/Videos")
-                    print(f"Downloading video to directory")
+                    print(f"Page: {name} has links, downloading content...")
                     for vid in obj.videos:
-                        print(f"Pretending to D/L {vid.course} video on {vid.page}:\n{vid.name} at {vid.url}")
+                        # print(f"Pretending to D/L {vid.course} video on {vid.page}:\n{vid.name} at {vid.url}")
+                        if self.formattype is "mp4":
+                            Downloader.download_as_video(vid, os.getcwd())
+                        elif self.formattype is "mp3":
+                            Downloader.download_as_mp3(vid, os.getcwd())
                         # download_function_not_yet_made(vid.name, vid.url)
         os.chdir(self.root_dir)
         #         else:
@@ -54,6 +60,7 @@ class DirMaker:
         #     print(f"{course_obj.name} has no pages, nothing to save!")
 
     def save_all(self, course_arr):
+
         for course in course_arr:
             self.save_course(course)
 
