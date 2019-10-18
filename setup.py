2f0ac90af5f23aa4b6cce32127f056209dc2c079
Stu Campbell <stucampbell.git@gmail.com>
2019-10-17 18:22:05 -0700

    update vers

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme(file='', split=False):
 
 setup(
     name='CanvasScraper',
-    version='0.5.0ev',
+    version='0.5.0dev',
     description='D/L Lectures/Data from Canvas',
     long_description=readme('README.md'),
     url='https://gitlab.com/stucamp/canvasscraper',
