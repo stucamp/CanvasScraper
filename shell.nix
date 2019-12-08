06354756f0de9b89b6779606237d15cd9b868b09
Stu Campbell <stucampbell.git@gmail.com>
2019-12-08 00:40:55 -0700

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,17 @@
+with import<nixpkgs> {};
+
+stdenv.mkDerivation rec {
+name = "env";
+
+  dependencies = [
+    python38
+    python38Packages.selenium
+    python38Packages.youtube-dl
+  ];
+
+  env = buildEnv {
+    name = name;
+    paths = dependencies;
+  };
+
+}
